bbbe13f28ea0737ec2bd067558241673d6dbcafc
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-04-25 16:32:34 +0000

    Automatically backed up by Learn

---
+++ lib/fancy_dance.rb
@@ -0,0 +1,24 @@
+
+# class FancyDance
+#   
+# def twirl
+# end
+# 
+# def jump
+# end
+# 
+# def pirouette
+# end
+# 
+# def take_a_bow
+# end
+# 
+# end
+
+ # FancyDance::ClassMethods module
+ #   has a metadata method (FAILED - 12)
+ #FancyDance::InstanceMethods module
+ # has a #twirl method (FAILED - 8)
+ #  has a #jump method (FAILED - 9)
+ #  has a #pirouette method (FAILED - 10)
+ #  has a #take_a_bow method (FAILED - 11)
