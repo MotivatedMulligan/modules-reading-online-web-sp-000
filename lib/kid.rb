e433d1edd99b5da3044173717e80a62b29373969
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-04-25 16:21:35 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,6 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+
+ # includes the Dance module (FAILED - 13)
+ # has a name (FAILED - 14)
+ # extends the MetaDancing module (FAILED - 15)
