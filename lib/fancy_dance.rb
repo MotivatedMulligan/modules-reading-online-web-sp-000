688ee979d5fdc63dcc5de3e44b4dd8b356151f9c
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-04-25 16:32:36 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -1,18 +1,18 @@
 
 # class FancyDance
-#   
+#
 # def twirl
 # end
-# 
+#
 # def jump
 # end
-# 
+#
 # def pirouette
 # end
-# 
+#
 # def take_a_bow
 # end
-# 
+#
 # end
 
  # FancyDance::ClassMethods module
