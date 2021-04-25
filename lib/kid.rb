b86c0b70fa52b876ab82f87ac8a7e80354e8b29a
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-04-25 16:27:34 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,6 +1,12 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 class Kid
+include Dance
+attr_accessor :name
+def initialize(name)
+  @name = name
+end
+  
 end
  # includes the Dance module (FAILED - 13)
  # has a name (FAILED - 14)
