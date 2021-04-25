56dbb9a5456fb01a7c92031191e23029745a94aa
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-04-25 16:26:37 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,9 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+
+class Dancer
+  
+end
+# includes the Dance module (FAILED - 5)
+# has a name (FAILED - 6)
+# extends the MetaDancing module (FAILED - 7)
