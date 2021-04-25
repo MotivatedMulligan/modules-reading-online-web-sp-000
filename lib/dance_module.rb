64df7687e0c15116bf47198eeec4bbbeb567096a
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-04-25 16:19:07 +0000

    Automatically backed up by Learn

---
+++ lib/dance_module.rb
@@ -0,0 +1,22 @@
+
+def dance_module
+  
+  def twirl 
+  end
+  
+  def jump
+  end
+  
+def pirouette
+end
+
+def take_a_bow
+end
+
+  
+end 
+
+# has a #twirl method (FAILED - 1)
+# has a #jump method (FAILED - 2)
+# has a #pirouette method (FAILED - 3)
+# has a #take_a_bow method (FAILED - 4)
