0ec1c8460d750ebb1c9707ad57ef8b6a578f5d1b
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-04-25 16:27:57 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,7 +2,13 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
-
+  include Dance
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
 end
 # includes the Dance module (FAILED - 5)
 # has a name (FAILED - 6)
