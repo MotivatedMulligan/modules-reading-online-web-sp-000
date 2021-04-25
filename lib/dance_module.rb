94825b5326978ff1aa88ced281a041097e50dc12
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-04-25 16:24:59 +0000

    Automatically backed up by Learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -1,20 +1,38 @@
-
-def dance_module
-  
+module Dance
   def twirl
+    "I'm twirling!"
   end
 
   def jump
+    "Look how high I'm jumping!"
   end
 
 def pirouette
+  "I'm doing a pirouette"
 end
 
 def take_a_bow
+  "Thank you, thank you. It was a pleasure to dance for you all."
 end
 
-
 end
+
+#def dance_module
+  
+#   def twirl
+#   end
+# 
+#   def jump
+#   end
+# 
+# def pirouette
+# end
+# 
+# def take_a_bow
+# end
+# 
+
+#end
 
 # has a #twirl method (FAILED - 1)
 # has a #jump method (FAILED - 2)
